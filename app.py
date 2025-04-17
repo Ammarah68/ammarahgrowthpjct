28508c67b600c857febfa264d187429a6fde42f1
Ammarah68 <161925645+Ammarah68@users.noreply.github.com>
2025-04-17 23:04:22 +0500

    Add files via upload
     Growth Mindset Challenge: Web App with Streamlit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(page_title="Growth mindset project", page_icon="★")
+st.title("♞ Growth Mindset Challenge: Web App with Streamlit")
+
+
+st.header("Welcome to growthmindset")
+st.markdown("### 💡 *Embrace challenges, learn from mistakes, and unlock your full potential.*")
+
+st.header("🌟 Today's Growth Mindset Quote")
+st.write("Success is not final, failure is not fatal: it is the courage to continue that counts. – Winston Churchill")
+
+# Reflection section
+st.info("Reflection on past experiences helps you grow! Share your thoughts.")
+
+# Achievements section
+st.info("Big or small, every achievement counts! Share one now.")
+
+#footer
+st.write("....")
+st.write("Keep believing in yourself. Growth is a journey, not a destination!")
+st.write("Created by Ammarah Moin")
+
+
